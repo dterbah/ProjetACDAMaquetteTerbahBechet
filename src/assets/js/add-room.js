f4ee5a4c3025556ebf5a63e206ef9b19cd3e5797
terbah <dorian.terb@gmail.com>
2018-10-03 19:29:39 +0200

    Add editable rooms

--- src/assets/js/add-room.js
+++ src/assets/js/add-room.js
@@ -1,6 +1,26 @@
 /*add a room */
-$(document).ready(function(){
-	$('.button-add-room').on('click', function(){
-		$('.container-room').append('<li style="border: solid 2px; border-radius: 10px; width: 50%; display: block; margin-right: auto; margin-left: auto;" class="w3-padding w3-margin-top"> Meuble IKEA <i style="float: right; cursor: pointer" class="w3-align-right fa fa-pencil"></i></li>');
+let index = 0;
+let onclick = 'window.location="review-of-furniture.html"'
+
+$(document).ready(function() {
+	//add a new li
+	$('.button-add-room').on('click', function() {
+		$('.container-room').append('<li contenteditable="true" id="li' + index + '" class="w3-padding w3-margin-top li-added"> Nom Salle <i  class="fa fa-pencil"></i></li>');
+		index++;
+		$('.container-room .li-added:last-child').focus(); //give the focus
+		/* add the listeners at added li*/
+		$('.container-room .li-added').on('focusout', function() {
+			var actualID = $(this).attr('id');
+			$('#' + actualID).attr('contenteditable', 'false');
+		});
+
+		$('.container-room .li-added:last-child i').on('click', function() {
+			$(location).attr('href', 'review-of-visit.html');	
+		});
+
+	});
+
+	$('.container-room li i').on('click', function() {
+		$(location).attr('href', 'review-of-visit.html');
 	});
 });
